1a7c15b0c7edd5da3ccd254c2377ebd0c4b941b0
RasssJ <rasmus.jalakas11@gmail.com>
2023-05-29 00:54:45 +0300

    Add code to SchedulePage.xaml.cs

--- MyConference/Pages/SchedulePage.xaml.cs
+++ MyConference/Pages/SchedulePage.xaml.cs
@@ -1,25 +1,45 @@
+using Microsoft.Maui.Controls;
 using MyConference.ViewModels;
 
 namespace MyConference.Pages;
+
+public partial class ScheduleDay1Page : SchedulePage
+{
+    public ScheduleDay1Page(ScheduleViewModel vm) : base()
+    {
+        vm.Day = 1;
+        Title = "Schedule - Day 1";
+        BindingContext = vm;
+    }
+}
+
+public partial class ScheduleDay2Page : SchedulePage
+{
+    public ScheduleDay2Page(ScheduleViewModel vm) : base()
+    {
+        vm.Day = 2;
+        Title = "Schedule - Day 2";
+        BindingContext = vm;
+    }
+}
 public partial class SchedulePage : ContentPage
 {
-	readonly ScheduleViewModel vm;
-    public static int day { get; set; }
-    public SchedulePage(ScheduleViewModel vm)
-	{
-		InitializeComponent();
+    ScheduleViewModel vm;
+    ScheduleViewModel VM => vm ??= BindingContext as ScheduleViewModel;
+    public SchedulePage()
+    {
+        InitializeComponent();
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        if (VM.Schedule.Count == 0)
+            await VM.LoadDataCommand.ExecuteAsync(null);
+    }
 
-        Day++;
-        vm.Day = Day;
-
-		BindingContext = this.vm = vm;
-
-	}
-
-    protected override async void OnNavigatedTo(NavigatedToEventArgs args)
+    protected override void OnNavigatedTo(NavigatedToEventArgs args)
     {
         base.OnNavigatedTo(args);
-		if (vm.Schedule.Count == 0)
-            await vm.LoadDataCommand.ExecuteAsync(null);
     }
+
 }
