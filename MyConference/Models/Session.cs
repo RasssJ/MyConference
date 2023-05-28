f769ecd4f74128c0a78689c3722445a6b670e378
RasssJ <rasmus.jalakas11@gmail.com>
2023-05-28 20:10:13 +0300

    Changes to Session.cs

--- MyConference/Models/Session.cs
+++ MyConference/Models/Session.cs
@@ -1,13 +1,22 @@
-﻿namespace MyConference.Models;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 
-public class Session
+namespace MyConference.Models;
+
+public partial class Session : ObservableObject
 {
+    // Static
     public int Id { get; set; }
     public string Title { get; set; }
     public string Room { get; set; }
     public string Description { get; set; }
+
     public DateTime Start { get; set; }
     public DateTime End { get; set; }
     public string StartTimeDisplay => $"{Start:t}";
     public string DayDisplay => $"{Start:MMM} {Start:d}";
+    public Speaker Speaker { get; set; }
+
+    // Dynamic
+    [ObservableProperty]
+    bool inAgenda;
 }
