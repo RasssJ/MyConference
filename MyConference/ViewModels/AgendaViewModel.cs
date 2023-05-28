fb25637d0653b1e1d46a88ecc66071cc293b14e8
RasssJ <rasmus.jalakas11@gmail.com>
2023-05-28 20:20:05 +0300

    Create GlobalUsings.cs

--- MyConference/ViewModels/AgendaViewModel.cs
+++ MyConference/ViewModels/AgendaViewModel.cs
@@ -1,6 +1,4 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using MvvmHelpers;
-using ObservableObject = CommunityToolkit.Mvvm.ComponentModel.ObservableObject
+﻿using MvvmHelpers;
 
  namespace MyConference.ViewModels;
 
