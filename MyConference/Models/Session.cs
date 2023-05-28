7a562669ebcd63333657fdc0c5721a965ee742f6
RasssJ <rasmus.jalakas11@gmail.com>
2023-05-28 23:07:07 +0300

    Add Jeffsum NuGet, changes to Session.cs, AgendaViewModel.cs

--- MyConference/Models/Session.cs
+++ MyConference/Models/Session.cs
@@ -14,7 +14,13 @@ public partial class Session : ObservableObject
     public DateTime End { get; set; }
     public string StartTimeDisplay => $"{Start:t}";
     public string DayDisplay => $"{Start:MMM} {Start:d}";
-    public Speaker Speaker { get; set; }
+    public Speaker Speaker { get; set; } = new Speaker
+    {
+        Name = "James Montemagno",
+        Company = "Microsoft",
+        Description = "James Montemagno is a Principal Lead Program Manager for .NET Community at Microsoft. He has been a .NET developer since 2005, working in a wide range of industries including game development, printer software, and web services. Prior to becoming a Principal Program Manager, James was a professional mobile developer and has now been crafting apps since 2011 with Xamarin. In his spare time, he is most likely cycling around Seattle or guzzling gallons of coffee at a local coffee shop.",
+        Title = "Principal Lead Program Manager"
+    };
 
     // Dynamic
     [ObservableProperty]
--- MyConference/ViewModels/AgendaViewModel.cs
+++ MyConference/ViewModels/AgendaViewModel.cs
@@ -1,13 +1,16 @@
 ﻿using CommunityToolkit.Mvvm.Input;
 using MvvmHelpers;
 using MyConference.Models;
+using Jeffsum;
+using static Jeffsum.Goldblum;
 
 namespace MyConference.ViewModels;
 
 public partial class AgendaViewModel : ObservableObject
 {
+    public int Day { get; set; }
     public ObservableRangeCollection<Grouping<string, Session>> Agenda { get; } = new();
-
+    Random random = new ();
     public AgendaViewModel() 
     {
     
@@ -16,6 +19,29 @@ public partial class AgendaViewModel : ObservableObject
     [RelayCommand]
     Task LoadDataAsync()
     {
+        var sessioncount = new[] { 1, 2, 4, 4, 4, 4, 4, };
+        var sessions = new List<Session>();
+        var start = new DateTime(2022, 9, Day, 8, 30, 0);
+
+        for (int i = 0; i < sessioncount.Length; i++)
+            AddItems(sessioncount[i], i);
+
         return Task.CompletedTask;
+
+        void AddItems(int count)
+        {
+            for (int i = 0; i < count; i++) 
+            {
+                sessions.Add(new Session
+                {
+                    Title = string.Join(" ", ReceiveTheJeff(random.Next(2, 5), JeffsumType.Words)),
+                    Description = ReceiveTheJeff(1).First(),
+                    Room = "Goldblum",
+                    Start = start.AddHours(offset),
+                    End = start.AddHours(offset + 1)
+                });
+
+            }
+        }
     }
 }
