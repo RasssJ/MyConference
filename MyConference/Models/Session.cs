f36f5d6374df320ccc0ce0fb2218739975231417
RasssJ <rasmus.jalakas11@gmail.com>
2023-05-28 19:58:49 +0300

    Add NuGet packages, Create Session.cs

---
+++ MyConference/Models/Session.cs
@@ -0,0 +1,13 @@
+﻿namespace MyConference.Models;
+
+public class Session
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Room { get; set; }
+    public string Description { get; set; }
+    public DateTime Start { get; set; }
+    public DateTime End { get; set; }
+    public string StartTimeDisplay => $"{Start:t}";
+    public string DayDisplay => $"{Start:MMM} {Start:d}";
+}
